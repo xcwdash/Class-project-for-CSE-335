0dd71024340c47613dfbd57a8825c0b1f7851e81
Zachary <zach.lee96@gmai.com>
2024-03-18 00:59:01 -0700

    Initial Commit

---
+++ Class project/Class_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Class_projectApp.swift
+//  Class project
+//
+//  Created by Zachary Lee on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Class_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
